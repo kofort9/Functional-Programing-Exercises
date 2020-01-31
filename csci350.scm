a86465cb3890f5745bd68bc505711382a6533945
Kofort9 <kofort9@gmail.com>
2020-01-30 21:13:03 -0500

    Clean up

--- csci350.scm
+++ csci350.scm
@@ -1,17 +1,9 @@
 ;Funtional Programming Assignment
 
 ; 1) reverse-general
-
-;define list L
-(define L
-  '(a b c)
-)
-(display L)
-(newline)
-
-;function reverse-general using recursion
-    ;parameter - a list
-;the result is a reversed version of the input list
+  ;function reverse-general using recursion
+  ;parameter - a list
+  ;the result is a reversed version of the input list
 
 (define (reverse-general L)
         (if (null? L)
